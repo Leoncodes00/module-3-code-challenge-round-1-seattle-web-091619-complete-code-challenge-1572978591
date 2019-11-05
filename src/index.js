cd7187b215bd9edebc46b9a4fb2ce456e5dfad06
Leoncodes00 <chachaiyang3@gmail.com>
2019-11-05 12:02:03 -0800

    committed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 3855;
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetchContents(imageURL);
 })
+
+function fetchContents(imageURL){
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then(json => {
+    displayContents(json);
+  })
+}
+
+function displayContents(response){
+  let imageCard = document.getElementById('image');
+  imageCard.src = response.url;
+  let titleImage = document.getElementById('name');
+  titleImage.textContent = response.name;
+  let likes = document.getElementById('likes');
+  likes.textContent = response.like_count;
+  let likeButton = document.getElementById('like_button');
+  likeButton.addEventListener('click', () => {
+    likes.textContent = response.like_count += 1
+    postRequest(response);
+  })
+  let commentForm = document.getElementById('comment_form');
+  let commentInput = document.getElementById('comment_input');
+  let commentList = document.getElementById('comments');
+  commentForm.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    let commentLi = document.createElement('li');
+    commentLi.textContent = commentInput.value;
+    commentList.appendChild(commentLi);
+  })
+
+}
+
+function postRequest(response){
+  let responseId = 3855;
+  fetch(`https://randopic.herokuapp.com/likes/`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify( {
+      like_count: response.like_count
+    })
+  })
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json);
+  })
+}
