49a621a37167633795468403d72245bb2946f329
Leoncodes00 <chachaiyang3@gmail.com>
2019-11-13 14:38:48 -0800

    Did some stuff and things

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ function displayContents(response){
   let likeButton = document.getElementById('like_button');
   likeButton.addEventListener('click', () => {
     likes.textContent = response.like_count += 1
-    postRequest(response);
+    postRequestLike(response);
   })
   let commentForm = document.getElementById('comment_form');
   let commentInput = document.getElementById('comment_input');
@@ -40,11 +40,12 @@ function displayContents(response){
     let commentLi = document.createElement('li');
     commentLi.textContent = commentInput.value;
     commentList.appendChild(commentLi);
+    postRequestComment(response);
   })
 
 }
 
-function postRequest(response){
+function postRequestLike(response){
   let responseId = 3855;
   fetch(`https://randopic.herokuapp.com/likes/`, {
     method: "POST",
@@ -53,7 +54,28 @@ function postRequest(response){
       Accept: 'application/json'
     },
     body: JSON.stringify( {
-      like_count: response.like_count
+      image_id: responseId
+    })
+  })
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json);
+  })
+}
+
+function postRequestComment(response){
+  let responseId = 3855;
+  let comments = response.comments;
+  
+  fetch(`https://randopic.herokuapp.com/comments/`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify( {
+      image_id: responseId,
+      content: response
     })
   })
   .then(resp => resp.json())
